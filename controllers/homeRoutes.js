1526096663978792d63ff1b3e639c65d7bfcefd3
Lauren Wollaston <laurenwollaston@outlook.com>
2023-06-02 22:27:26 -0500

    A lot of work on routes. Besides logins, there are profiles and userpages. More polish on the navbuttons and navbar. Logout button is now on the right hand side of the screen to prevent accidental logout.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,12 +27,79 @@ router.get('/', async (req, res) => {
   }
 });
 
-
+router.get('/dashboard', async (req, res) => {
+  if (req.session.loggedIn){
+  try {
+    const postData = await Post.findAll({
+      where: { poster_id: req.session.user_id},
+    });
+    const userData = await User.findOne({where: {id: req.session.user_id}});
+    const user = userData.get({ plain: true });
+    const posts = await postData.map((post) =>
+      post.get({ plain: true })
+    );
+    var personalDashboard = true;
+    res.render('dashboard', {
+      posts, user,
+      loggedIn: req.session.loggedIn,
+      personalDashboard,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  }
+  else{
+    res.redirect(`/login`);
+  }
+});
 
 router.get('/login', (req, res) => {
   res.render('login', { loggedIn: req.session.loggedIn });
 });
+router.get('/user/:username', async (req, res) => {
+  try {
+    const { username } = req.params;
+    const userData = await User.findOne({ where: { username } });
+    const user = userData.get({ plain: true });
+    const postData = await Post.findAll({
+      where: { poster_id: user.id},
+    });
+    const posts = await postData.map((post) =>
+      post.get({ plain: true })
+    );
+    if (!userData) {
+      res.status(404).render('error', { message: 'User profile not found', loggedIn: req.session.loggedIn });
+      return;
+    }
+    res.render('dashboard', {
+      posts, user,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-
+router.get('/profile', async (req, res) => {
+  if(req.session.loggedIn){
+    try {
+      const username = req.session.user_id;
+      const userData = await User.findOne({ where: username});
+      if (!userData) {
+        res.status(404).render('error', { message: 'User not found', loggedIn: req.session.loggedIn });
+        return;
+      }
+      const user = userData.get({ plain: true });
+      res.render('profile', { user, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+} else{
+  res.redirect('/login');
+}
+});
 
 module.exports = router;
